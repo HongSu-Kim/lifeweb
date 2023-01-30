d8ec592cf801ed6c0ab3809a44146a0184f532ba
biber <dlehdxo2@gmail.com>
2023-01-30 18:18:33 +0900

    SNS이름으로 MembeSns를 찾을수있는 메소드 추가

--- src/main/java/com/bethefirst/lifeweb/repository/member/MemberSnsRepository.java
+++ src/main/java/com/bethefirst/lifeweb/repository/member/MemberSnsRepository.java
@@ -3,5 +3,10 @@ package com.bethefirst.lifeweb.repository.member;
 import com.bethefirst.lifeweb.entity.member.MemberSns;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberSnsRepository extends JpaRepository<MemberSns, Long> {
+    boolean existsBySnsUrl(String snsUrl);
+
+    Optional<MemberSns> findBySns_Name(String snsName);
 }
