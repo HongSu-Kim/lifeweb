c7d390ce4da54b20601507c9b79a8f01dae8adc9
biber <dlehdxo2@gmail.com>
2023-01-30 18:14:50 +0900

    토큰 검증 실패시 일단 이셉션터트리기 (예외 캐치할곳 수정 필요)

--- src/main/java/com/bethefirst/lifeweb/config/security/JwtFilter.java
+++ src/main/java/com/bethefirst/lifeweb/config/security/JwtFilter.java
@@ -1,18 +1,17 @@
 package com.bethefirst.lifeweb.config.security;
 
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.util.StringUtils;
-import org.springframework.web.filter.GenericFilterBean;
-
+import com.bethefirst.lifeweb.exception.UnauthorizedException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.GenericFilterBean;
+
 import java.io.IOException;
 
 @Slf4j
@@ -36,7 +35,7 @@ public class JwtFilter extends GenericFilterBean {
             SecurityContextHolder.getContext().setAuthentication(authentication);
             log.debug("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
         } else {
-            log.debug("유효한 JWT 토큰이 없습니다, uri: {}", requestURI);
+            throw new UnauthorizedException("유효한 JWT 토큰이 없습니다, uri: " + requestURI.toString());
         }
 
         filterChain.doFilter(servletRequest, servletResponse);
