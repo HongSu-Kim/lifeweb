1b71d8ef71cd9e54fbec717434d196392c96158e
biber <dlehdxo2@gmail.com>
2023-01-31 23:59:51 +0900

    리뷰 등록 구현완료 / 리뷰 삭제 서비스계층 구현중

--- src/main/java/com/bethefirst/lifeweb/service/review/interfaces/ReviewService.java
+++ src/main/java/com/bethefirst/lifeweb/service/review/interfaces/ReviewService.java
@@ -1,4 +1,12 @@
 package com.bethefirst.lifeweb.service.review.interfaces;
 
+import com.bethefirst.lifeweb.dto.review.reqeust.ReviewCreateDto;
+
 public interface ReviewService {
+
+    /** 리뷰 등록 */
+    void createReview(ReviewCreateDto reviewCreateDto, Long memberId);
+
+    /** 리뷰 삭제 */
+    void deleteReview(Long reviewId);
 }
