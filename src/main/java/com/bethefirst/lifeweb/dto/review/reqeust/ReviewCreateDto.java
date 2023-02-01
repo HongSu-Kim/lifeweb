7641b5df3e26cab48d1db551907344c610a4bb60
biber <dlehdxo2@gmail.com>
2023-02-01 18:30:53 +0900

    리뷰 생성자 구조 변경 createDto에서 생성자를 사용하여 리뷰를 반환하는 구조로 변경

--- src/main/java/com/bethefirst/lifeweb/dto/review/reqeust/ReviewCreateDto.java
+++ src/main/java/com/bethefirst/lifeweb/dto/review/reqeust/ReviewCreateDto.java
@@ -1,5 +1,8 @@
 package com.bethefirst.lifeweb.dto.review.reqeust;
 
+import com.bethefirst.lifeweb.entity.campaign.Campaign;
+import com.bethefirst.lifeweb.entity.member.Member;
+import com.bethefirst.lifeweb.entity.review.Review;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,5 +12,9 @@ public class ReviewCreateDto {
 
     private Long campaignId;
     private String reviewUrl;
+
+    public Review createReview(Member member, Campaign campaign){
+        return new Review(member, campaign, reviewUrl);
+    }
 
 }
