ea1292ee90ea3f5686e235637f2db8f7a082d001
HS <coldghd@gmail.com>
2023-02-17 21:02:44 +0900

    📝 캠페인 검색 조건 추가
    pick, applicantStatus, memberId

--- src/main/java/com/bethefirst/lifeweb/dto/campaign/request/CampaignSearchRequirements.java
+++ src/main/java/com/bethefirst/lifeweb/dto/campaign/request/CampaignSearchRequirements.java
@@ -1,5 +1,6 @@
-package com.bethefirst.lifeweb.dto.campaign;
+package com.bethefirst.lifeweb.dto.campaign.request;
 
+import com.bethefirst.lifeweb.entity.application.ApplicantStatus;
 import com.bethefirst.lifeweb.entity.campaign.CampaignStatus;
 import lombok.*;
 import org.springframework.data.domain.Pageable;
@@ -11,14 +12,18 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@EqualsAndHashCode
 public class CampaignSearchRequirements {//캠페인 검색 조건
 
 	private Pageable pageable;//페이징
 	private Long categoryId;//카테고리
 	private Long typeId;//타입
-	private List<Long> snsIdList;//SNS
+	private List<Long> snsId;//SNS
 	private Boolean special;//스페셜
-	private CampaignStatus status;//상태
+	private Boolean pick;//픽
+	private CampaignStatus campaignStatus;//캠페인상태
 	private Long localId;//지역
+	private ApplicantStatus applicantStatus;//신청자상태
+	private Long memberId;//회원
 
 }
