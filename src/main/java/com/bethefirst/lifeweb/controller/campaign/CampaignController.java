b2eb396f296740e011f205bbcb9bad88f489a0c4
HS <coldghd@gmail.com>
2023-02-20 20:49:58 +0900

    ✨ 캠페인 기능 추가
    1. 캠페인 상태 변경
    2. 캠페인 PICK 체크

--- src/main/java/com/bethefirst/lifeweb/controller/campaign/CampaignController.java
+++ src/main/java/com/bethefirst/lifeweb/controller/campaign/CampaignController.java
@@ -1,13 +1,8 @@
 package com.bethefirst.lifeweb.controller.campaign;
 
+import com.bethefirst.lifeweb.dto.campaign.request.*;
 import com.bethefirst.lifeweb.dto.campaign.response.CampaignDto;
-import com.bethefirst.lifeweb.dto.campaign.request.CampaignSearchRequirements;
-import com.bethefirst.lifeweb.dto.campaign.request.CreateCampaignDto;
-import com.bethefirst.lifeweb.dto.campaign.request.UpdateCampaignDto;
-import com.bethefirst.lifeweb.exception.ForbiddenException;
-import com.bethefirst.lifeweb.exception.UnauthorizedException;
 import com.bethefirst.lifeweb.service.campaign.interfaces.CampaignService;
-import com.bethefirst.lifeweb.util.security.SecurityUtil;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,10 +13,10 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("campaigns")
@@ -56,15 +51,6 @@ public class CampaignController {
 	public Page<CampaignDto> readAll(CampaignSearchRequirements searchRequirements,
 									 @PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
 
-		if (searchRequirements.getMemberId() != null) {
-			Optional<Long> currentMemberId = SecurityUtil.getCurrentMemberId();
-			Long memberId = currentMemberId
-					.orElseThrow(() -> new UnauthorizedException("Security Context에 인증 정보가 없습니다."));
-			if (!searchRequirements.getMemberId().equals(memberId)) {
-				throw new ForbiddenException("권한 없음");
-			}
-		}
-
 		searchRequirements.setPageable(pageable);
 		return campaignService.getCampaignDtoPage(searchRequirements);
 	}
@@ -84,10 +70,19 @@ public class CampaignController {
 		return new ResponseEntity<>(headers, HttpStatus.CREATED);
 	}
 	
-//	/** 캠페인 상태 변경 */
-//	@PutMapping("/{campaignId}/status")
-//	public void updateStatus(@PathVariable Long campaignId, @Valid @NotEmpty(message = "상태는 필수 입력 값입니다.") CampaignStatus status) {
-//		campaignService.updateStatus(campaignId, status);
-//	}
+	/** 캠페인 상태 변경 */
+	@ResponseStatus(HttpStatus.CREATED)
+	@PutMapping("/{campaignId}/status")
+	public void updateStatus(@PathVariable Long campaignId,
+							 @Valid @RequestBody CampaignStatusDto campaignStatusDto) {
+		campaignService.updateStatus(campaignId, campaignStatusDto.getStatus());
+	}
+
+	/** 캠페인 PICK 체크 */
+	@ResponseStatus(HttpStatus.CREATED)
+	@PutMapping("/pick")
+	public void updatePick(@RequestBody UpdateCampaignPickDto updateCampaignPickDto) {
+		campaignService.updatePick(updateCampaignPickDto);
+	}
 
 }
--- src/test/java/com/bethefirst/lifeweb/ControllerTest.java
+++ src/test/java/com/bethefirst/lifeweb/ControllerTest.java
@@ -1,6 +1,8 @@
 package com.bethefirst.lifeweb;
 
+import com.bethefirst.lifeweb.dto.CustomUser;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,6 +16,9 @@ import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
 import org.springframework.restdocs.snippet.Attributes.Attribute;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMultipartHttpServletRequestBuilder;
@@ -23,24 +28,23 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 import java.beans.PropertyDescriptor;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static com.bethefirst.lifeweb.RestDocsConfig.field;
 
-//@WithMockUser
 @Import(RestDocsConfig.class)
 @ExtendWith(RestDocumentationExtension.class)
 @Slf4j
-public class ControllerTest {
+public abstract class ControllerTest {
+
 	@Autowired
 	protected MockMvc mockMvc;
 	@Autowired
 	protected RestDocumentationResultHandler restDocs;
-
 	@Autowired
 	protected ObjectMapper objectMapper;
+	protected CustomUser user;
+
 
 	@BeforeEach
 	void setUp(final WebApplicationContext context,
@@ -54,39 +58,52 @@ public class ControllerTest {
 				.build();
 	}
 
-	protected MockHttpServletRequestBuilder createParamsRequest(
-			MockHttpServletRequestBuilder request,
-			Object dto) {
+	protected String getJwt(String role, Long memberId) {
 
-		//Request에 DTO의 모든 속성,값 정보 추가
-		final PropertyDescriptor[] getterDescriptors = ReflectUtils.getBeanGetters(dto.getClass());
-		for (PropertyDescriptor pd : getterDescriptors) {
-			try {
-				//파라미터로 전달한 인스턴스에 대해 getter함수를 호출한 값 얻어옴
-				Object invoke = pd.getReadMethod().invoke(dto);
-				if (invoke instanceof List<?>) {
-					((List<?>) invoke).forEach(o -> param(request,pd.getName(), o));
-				} else {
-					param(request, pd.getName(), invoke);
-				}
-			} catch (Exception e) {
-				log.error(e.toString());
-			}
-		}
-		return request;
+		user = new CustomUser("username", "password", Arrays.asList(new SimpleGrantedAuthority(role)), memberId);
+		PreAuthenticatedAuthenticationToken auth = new PreAuthenticatedAuthenticationToken(user, null, user.getAuthorities());
+
+		SecurityContextHolder.getContext().setAuthentication(auth);
+		return Jwts.builder()
+				.setSubject(user.getUsername())
+				.claim("memberId", user.getMemberId())
+				.claim("role", user.getAuthorities())
+				.compact();
 	}
 
-	private MockHttpServletRequestBuilder param(
-			MockHttpServletRequestBuilder request,
-			String name,
-			Object value) {
-
-		if ((value instanceof String || value instanceof Number || value instanceof Boolean || value instanceof Enum<?>)
-				&& (!String.valueOf(value).equals("null"))) {
-			request.param(name, String.valueOf(value));
-		}
-		return request;
-	}
+//	protected MockHttpServletRequestBuilder createParamsRequest(
+//			MockHttpServletRequestBuilder request,
+//			Object dto) {
+//
+//		//Request에 DTO의 모든 속성,값 정보 추가
+//		final PropertyDescriptor[] getterDescriptors = ReflectUtils.getBeanGetters(dto.getClass());
+//		for (PropertyDescriptor pd : getterDescriptors) {
+//			try {
+//				//파라미터로 전달한 인스턴스에 대해 getter함수를 호출한 값 얻어옴
+//				Object invoke = pd.getReadMethod().invoke(dto);
+//				if (invoke instanceof List<?>) {
+//					((List<?>) invoke).forEach(o -> param(request,pd.getName(), o));
+//				} else {
+//					param(request, pd.getName(), invoke);
+//				}
+//			} catch (Exception e) {
+//				log.error(e.toString());
+//			}
+//		}
+//		return request;
+//	}
+//
+//	private MockHttpServletRequestBuilder param(
+//			MockHttpServletRequestBuilder request,
+//			String name,
+//			Object value) {
+//
+//		if ((value instanceof String || value instanceof Number || value instanceof Boolean || value instanceof Enum<?>)
+//				&& (!String.valueOf(value).equals("null"))) {
+//			request.param(name, String.valueOf(value));
+//		}
+//		return request;
+//	}
 
 	protected MockHttpServletRequestBuilder createMultiPartRequest(
 			MockMultipartHttpServletRequestBuilder multipartRequest,
@@ -157,6 +174,10 @@ public class ControllerTest {
 
 	protected Attribute type(Object value) {
 		return field("type", value.toString());
+	}
+
+	protected Attribute role(Object value) {
+		return field("role", value.toString());
 	}
 
 }
