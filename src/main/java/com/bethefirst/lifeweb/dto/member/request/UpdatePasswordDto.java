418ba0a5741d6aaba0702324b1dbe907500a88ee
biber <dlehdxo2@gmail.com>
2023-02-21 18:51:14 +0900

    클래스이름변경 PasswordDto -> updatePasswordDto

---
+++ src/main/java/com/bethefirst/lifeweb/dto/member/request/UpdatePasswordDto.java
@@ -0,0 +1,18 @@
+package com.bethefirst.lifeweb.dto.member.request;
+
+import com.bethefirst.lifeweb.entity.member.Member;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Getter
+@Setter
+public class UpdatePasswordDto {
+
+    private String newPassword;
+    private String confirmPassword;
+
+    public void updatePassword(PasswordEncoder passwordEncoder, Member member){
+        member.updatePassword(passwordEncoder, newPassword);
+    }
+}
